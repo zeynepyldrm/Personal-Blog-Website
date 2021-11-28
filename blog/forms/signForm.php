e7796bf22ec0804b6e17070a3a3d6916ac428975
mrdlypenyez <zeynepyldrm1516@gmail.com>
2021-11-28 20:17:55 +0300

    template changed and js file resize

--- blog/forms/signForm.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$name = $_REQUEST['usernameInput'];
-echo "<script type='text/javascript'>alert($name);</script>";
-?>
---
+++ forms/signForm.php
@@ -0,0 +1,8 @@
+<?php
+
+if ($_POST){ //sayfa post edildiyse
+    $dizi['result'] = 'Post Edildi. Veriler console ekranına yazdırıldı';
+
+}
+
+?>
